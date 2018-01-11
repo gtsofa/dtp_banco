0a9f04a2c9c5b427a06a3c775d74fab47737b2e9
Julius Nyule <jlsnyule22@gmail.com>
2018-01-11 19:33:30 +0000

    add code to bank class

--- Banco/bank/test/bank_test.py
+++ Banco/bank/test/bank_test.py
@@ -7,5 +7,11 @@ from bank.bank import Bank
 class TestBankClass(unittest.TestCase):
     """Test bank class """
     
+    def test_bank_is_initially_empty(self):
+        """Test bank if it is empty"""
+        bank = Bank()
+        self.assertEqual({}, bank.accounts)
+        self.assertEqual(len(bank.accounts), 0)
+    
 if __name__ == '__main__':
     unittest.main()
