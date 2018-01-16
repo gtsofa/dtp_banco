6cc0b7c6bb83348a57144858e596aae34460a950
Julius Nyule <jlsnyule22@gmail.com>
2018-01-16 20:50:13 +0000

    refactor

--- Banco/bank/account.py
+++ Banco/bank/account.py
@@ -6,3 +6,13 @@ class Account():
         """initialize parameters"""
         self.account_number = account_number
         self.account_balance = account_balance
+        
+    def add_account(self):
+        """this add bank account of an individual"""
+        
+        print("Account number "+ self.account_number +" has Kshs."+ str(self.account_balance) + " only as balance")
+        
+account = Account('001', 20)
+accountb = Account('002', 30)
+account_1 = account.add_account()
+account_2 = accountb.add_account()
--- Banco/bank/bank.py
+++ Banco/bank/bank.py
@@ -1,5 +1,41 @@
 #bank class
+"""
+accounts = {
+  'tsofa nyule': {
+    'account_number':'001',
+    'account_balance':50
+  },
+  'sarah munga': {
+    'account_number':'002',
+    'account_balance':300
+  },
+  'maestro giulio': {
+    'account_number': '003',
+    'account_balance': 720
+  }
+  
+}
+
+for user, account in accounts.items():
+  print(user +"..> " "Account number: "+ account['account_number'] + " Account balance: " + str(account['account_balance']))
+  
+
+"""
 
 class Bank():
-    """A class that models bank accounts and current balance"""
-    pass
+    """Bank class that stores All Accounts in a dictionary."""
+    
+    def __init__(self):
+        """initialize bank class"""
+        self.accounts = {} #empty dictionary that will contain all the accounts
+        
+    def add_account(self, account):
+        """add individual account to the bank class"""
+        self.accounts[account.account_number] = account.account_balance
+        
+    def get_account_balance(self, account_number):
+        """return account balance"""
+        return self.accounts.get(account_number)
+        
+        
+        
